71416335f42fffa1a736fc6f645488881fb4383b
joecosta117 <joecosta117@gmail.com>
2019-03-14 20:56:19 -0400

    final example

--- react-hooks-ex.js
+++ react-hooks-ex.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react'
 
 export default function Puppy() {
   const [name, setName] = useState('Doggo')
-  const [age, setAge] = useState(4)
-  //.......
+
+  return (
+    <div>
+      <h1>The puppy's name is {name}</h1>
+    </div>
+  )
 }
 
 
@@ -14,14 +18,19 @@ import React, { useState, useEffect } from 'react'
 export function Puppy() {
   const [name, setName] = useState('Doggo')
   useEffect(() =>{
-    document.title = 'Puppy Name: ' + this.state.name
+    document.title = 'Puppy Name: ' + name
   })
 
   const [age, setAge] = useState(4)
+  useEffect(() => {
+    console.log('second useEffect!')
+    return () => console.log('unmounting...')
+  })
 
   return (
     <div>
-      <h1>The puppy's name is {this.state.name}</h1>
+      <h1>The puppy's name is {name}</h1>
+      <h3>The puppy's age is {age}</h3>
     </div>
   )
 }
